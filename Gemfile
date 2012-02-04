4c014dd9dcd3bf12f43da573eeb0a96c4402a2e6
Cora Korsman <corakorsman@gmail.com>
2012-02-04 16:51:59 +0100

    Add validation to ensure names are specified when creating projects

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'dynamic_form'
+
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,22 @@
 class ProjectsController < ApplicationController
 	def index
 	end
+  
 	def new
 		@project = Project.new
 	end
+  
 	def create
 		@project = Project.new(params[ :project])
-		@project.save
-		flash[:notice] = "Project has been created."
-		redirect_to @project
+	  if @project.save
+		  flash[:notice] = "Project has been created."
+		  redirect_to @project
+    else
+      flash[:alert] = "Project has not been created."
+      render :action => "new"
+    end
 	end
+  
 	def show
 		@project = Project.find(params[:id])
 	end
