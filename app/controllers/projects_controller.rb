8b4828d7072497c140ce422af0b1a628fe0b89ce
Cora Korsman <corakorsman@gmail.com>
2012-02-09 15:19:08 +0100

    Implemented delete functionality for projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,5 +36,12 @@ class ProjectsController < ApplicationController
       render :action => "edit"
     end
   end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
 	
 end
