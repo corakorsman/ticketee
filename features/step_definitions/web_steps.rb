14ea4388dc8f29426f19271629506d429710532a
Cora Korsman <corakorsman@gmail.com>
2012-03-08 01:41:04 +0100

    Fixes #1: prevent deprecation warnings in viewing_tickets feature
    A step definition in web_steps used an outdated way of calling other
    steps in step_definitions/web_steps.rb ('Given / When / Then'-style
    instead of 'step')

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -32,8 +32,8 @@ end
 World(WithinHelpers)
 
 # Single-line step scoper
-When /^(.*) within (.*[^:])$/ do |step, parent|
-  with_scope(parent) { When step }
+When /^(.*) within (.*[^:])$/ do |the_step, parent|
+  with_scope(parent) { step the_step }
 end
 
 # Multi-line step scoper
