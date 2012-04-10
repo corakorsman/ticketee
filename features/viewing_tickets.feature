86025817b6528355c22faf38c1a38c753f297b33
Cora Korsman <corakorsman@gmail.com>
2012-04-10 14:43:33 +0200

    Made projects only visible to users with permission to see them

--- features/viewing_tickets.feature
+++ features/viewing_tickets.feature
@@ -7,11 +7,14 @@ Feature: Viewing tickets
     Given there are the following users:
       |email              |password |
       |user@ticketee.com  |password |
+    And I am signed in as them
     And there is a project called "TextMate 2"
+    And "user@ticketee.com" can view the "TextMate 2" project
     And "user@ticketee.com" has created a ticket for this project:
       |title          |description                   |
       |Make it shiny! |Gradients! Starbursts! Oh my! |
     And there is a project called "Internet Explorer"
+    And "user@ticketee.com" can view the "Internet Explorer" project
     And "user@ticketee.com" has created a ticket for this project:
       |title                |description   |
       |Standards compliance |Isn't a joke. |
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,15 +2,24 @@ require 'spec_helper'
 
 describe ProjectsController do
 #  let(:user) do
-#    user = Factory(:user)
+#      user = Factory(:user)
 #    user.confirm!
 #    user
 #  end
+
   let(:user) { create_user! }
   
   let(:project) { Factory(:project) }
   
   context "standard users" do
+  
+    it "cannot access the show action" do
+      sign_in(:user, user)
+      get :show, :id => project.id
+      response.should redirect_to(projects_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+    
    {  "new" => "get",
       "create" => "post",
       "edit" => "get",
@@ -23,9 +32,10 @@ describe ProjectsController do
         flash[:alert].should eql("You must be an admin to do that.")
       end
     end
-  end
+  end    
   
     it "displays an error for a missing project" do
+      sign_in(:user, user)
       get :show, :id => "not-here"
       response.should redirect_to(projects_path)
       message = "The project you were looking for could not be found."
