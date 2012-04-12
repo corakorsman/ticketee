b3175b158b9d7f5516b3a6215114eb60c6d6e392
Cora Korsman <corakorsman@gmail.com>
2012-04-12 15:41:00 +0200

    Added permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,80 +1,24 @@
 Ticketee::Application.routes.draw do
-  
-# verwijderen zie pagina 148-Rails 3 in action
-# get "users/index"
-  
+
 	root :to => "projects#index"
 	resources :projects do
-    resources :tickets 
+    resources :tickets
   end
   
   namespace :admin do
     root :to => "base#index"
-    resources :users 
+    resources :users do
+      resources :permissions
+    end
   end
-  
   
   devise_for :users, :controllers => { :registrations => "registrations" }
 
   get '/awaiting_confirmation',
-  :to => "users#confirmation",
-  :as => 'confirm_user'
+    :to => "users#confirmation",
+    :as => 'confirm_user'
   
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
- 
+  put '/admin/users/:user_id/permissions',
+    :to => 'admin/permissions#update',
+    :as => :update_user_permissions
 end
