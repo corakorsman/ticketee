636e6ab59a968e101b3b5e49ef15fc50408a089d
Cora Korsman <corakorsman@gmail.com>
2012-04-25 23:34:50 +0200

    Provide an 'Add another link' that uses Javascript so that users can upload more than one file

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,7 +13,7 @@ class TicketsController < ApplicationController
  
   def new
     @ticket = @project.tickets.build
-    3.times { @ticket.assets.build }
+    @ticket.assets.build
   end
   
   def create
