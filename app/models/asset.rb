ec7d5133a16675aafb79d6341cbde8f74eef3019
Cora Korsman <corakorsman@gmail.com>
2012-04-25 11:57:12 +0200

    Assets are now strickly served through FilesController

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,5 @@
 class Asset < ActiveRecord::Base
-  has_attached_file :asset
+  has_attached_file :asset, :path => (Rails.root + "files/:id").to_s,
+                            :url => "/files/:id"
+  belongs_to :ticket
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Ticketee::Application.routes.draw do
   put '/admin/users/:user_id/permissions',
     :to => 'admin/permissions#update',
     :as => :update_user_permissions
+    
+  resources :files
 end
