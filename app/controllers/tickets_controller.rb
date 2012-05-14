4763be650c6dab9b97a15f5b9b97fac92051a29e
Cora Korsman <corakorsman@gmail.com>
2012-05-14 11:58:19 +0200

    when updating a comment's status, it also updates the ticket's status

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -29,6 +29,7 @@ class TicketsController < ApplicationController
 
   def show
     @comment = @ticket.comments.build
+    @states = State.all
   end
     
   def edit
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,26 @@
 class Comment < ActiveRecord::Base
-  validates :text, :presence => true
+  after_create :set_ticket_state
+  
+  belongs_to :ticket
   belongs_to :user
+  belongs_to :state
+  
+  delegate :project, :to => :ticket
+  
+  validates :text, :presence => true
+  
+  private
+  
+#    def set_previous_state
+#      self.previous_state = ticket.state
+#    end
+
+    def set_ticket_state
+      self.ticket.state = self.state
+      self.ticket.save!
+    end
+    
+#    def creator_watches_ticket
+#      ticket.watchers << user
+#    end
 end
