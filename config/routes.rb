fc4784896746a02893c6eb6609314b9f3bf5dfb2
Cora Korsman <corakorsman@gmail.com>
2012-05-15 10:38:44 +0200

    Admins can now set a default state for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Ticketee::Application.routes.draw do
     resources :users do
       resources :permissions
     end
-    resources :states
+    resources :states do
+      member do
+        get :make_default
+      end
+    end
   end
   
   devise_for :users, :controllers => { :registrations => "registrations" }
